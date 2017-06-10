b7019438d5bea0728e936acd9c7dd20e28dc3fc7
kia laptop <kiaxar2000@gmail.com>
2017-06-10 16:04:11 +0430

    bug fixed

--- models/display.js
+++ models/display.js
@@ -2,25 +2,19 @@
 /**
  * display control
  */
-var abringPageShow = function (page,title,message,template_page_id) {
+var abringPageShow = function (page,title,message) {
 
     $("#abring").show();
     $("#abring>div").hide();
     $("#abring>div.modal_background").show();
 
-    template_page_id = template_page_id || abring.params.display.default_page_id;
-
-    var content = $("."+page).outerHTML();
-    $("#abring #"+template_page_id+" .content-div").html(content);
-
     title = title || "";
-    $("#abring #"+template_page_id+" .abring-dialog-title").html(title);
+    $("#abring ."+page+" .abring-dialog-title").html(title);
 
     message = message || "";
-    $("#abring #"+template_page_id+" .abring-dialog-message").html(message);
+    $("#abring ."+page+" .abring-dialog-message").html(message);
 
-    $("#abring #"+template_page_id+" ."+page).show();
-    $("#abring #"+template_page_id).show();
+    $("#abring ."+page).show();
 };
 var abringPageHide = function () {
     $("#abring").hide();
