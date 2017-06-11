fd80b795eb6f8824ec22ff863e200eda1d720eab
kia <kiaxar2000@gmail.com>
2017-06-11 14:25:26 +0430

    delete from basket

--- modules/market/controller/basket.js
+++ modules/market/controller/basket.js
@@ -24,6 +24,28 @@ $(document).on("click",".abring_button_market_add_to_basket",function () {
         }
     );
 });
+$(document).on("click",".abring_button_market_delete_from_basket",function () {
+
+    if(!abring.params.player_info)
+    {
+        abring.params.display.error.show("error","Your are not login!");
+        return false;
+    }
+
+    var product_id = $(this).attr("product_id");
+    var amount = 0;
+
+    addToBasket(
+        product_id,
+        amount,
+        function (basket) {
+            fillBasketView(basket);
+            abring.params.market.basket.pages.basketView.show();
+        },function (x,c,e) {
+            abring.params.display.error.show(e);
+        }
+    );
+});
 $(document).on("click",".abring_buttons_market_basket_view",function () {
 
     if(!abring.params.player_info)
