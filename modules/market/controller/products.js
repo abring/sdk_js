281ba69a0dcf2383a756e6fc197a28bf7dd70e17
kia <kiaxar2000@gmail.com>
2017-06-11 12:43:38 +0430

    update

--- modules/market/controller/products.js
+++ modules/market/controller/products.js
@@ -8,6 +8,9 @@ $(document).on("click",".abring_buttons_view_products_list",function () {
         "products/list",
         {"limit":10,"offset":0,"tags":"*"},
         function (products) {
+            $.each(products,function (index,product) {
+                products[index]["data"] = JSON.parse(product["data"]);
+            });
             abring.params.market.products.pages.productList.show(products);
         },function (x,c,e) {
             abring.params.display.error.show(e);
