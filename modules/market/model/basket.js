89feeba3be87a9a204f376a84ee5b1a2e71a26aa
saeed <saeed.ahmadi.it@gmail.com>
2017-06-14 14:08:45 +0430

    basket and perchase list

--- modules/market/model/basket.js
+++ modules/market/model/basket.js
@@ -8,15 +8,15 @@ var fillBasketView = function(basket,parent_id){
     abring.params.market.basket.pages.basketView.getTheme();
     if(!parent_id)
         parent_id = abring.params.market.basket.pages.basketView.parent_id;
-    var list_row_template = $("."+parent_id+" ul li:first-child").outerHTML();
+    var list_row_template = $("."+parent_id+" table tr.template").outerHTML();
     $.each(basket["products"],function (index,product) {
-        $("."+parent_id+" ul").append(list_row_template);
-        $("."+parent_id+" ul li:last").attr("product_id",product["id"]);
-        $("."+parent_id+" ul li:last *").attr("product_id",product["id"]);
-        $("."+parent_id+" ul li:last .image").attr("src",product["image"]).addClass("abring_buttons_view_product_details");
-        $("."+parent_id+" ul li:last .name").html(product["name"]).addClass("abring_buttons_view_product_details");
-        $("."+parent_id+" ul li:last .fee").html(product["fee"]);
-        $("."+parent_id+" ul li:last .order").html(product["order"]);
+        $("."+parent_id+" table").append(list_row_template);
+        $("."+parent_id+" table tr:last-child td").attr("product_id",product["id"]);
+        $("."+parent_id+" table tr:last-child * td").attr("product_id",product["id"]);
+        $("."+parent_id+" table tr:last-child td .image").attr("src",product["image"]).addClass("abring_buttons_view_product_details");
+        $("."+parent_id+" table tr:last-child td .name").html(product["name"]).addClass("abring_buttons_view_product_details");
+        $("."+parent_id+" table tr:last-child td .fee").html(product["fee"]);
+        $("."+parent_id+" table tr:last-child td .order").val(product["order"]);
     })
 };
 var addToBasket = function(product_id,amount,addToBasketSuccess,addToBasketFailed){
@@ -51,14 +51,14 @@ var getBaskets = function(getBasketsSuccess)
 var fillBasketList = function(baskets){
     abring.params.market.basket.pages.basketList.getTheme();
     var parent_id = abring.params.market.basket.pages.basketList.parent_id;
-    var list_row_template = $("."+parent_id+" ul li:first-child").outerHTML();
+    var list_row_template = $("."+parent_id+" table tr.template").outerHTML();
     $.each(baskets,function (index,basket) {
-        $("."+parent_id+" ul").append(list_row_template);
-        $("."+parent_id+" ul li:last .create_time").html(basket["create_time"]);
-        $("."+parent_id+" ul li:last .fee").html(basket["fee"]);
-        $("."+parent_id+" ul li:last .status").html(basket["status"]);
+        $("."+parent_id+" table").append(list_row_template);
+        $("."+parent_id+" table tr:last-child td .create_time").html(basket["create_time"]);
+        $("."+parent_id+" table tr:last-child td .fee").html(basket["fee"]);
+        $("."+parent_id+" table tr:last-child td .status").html(basket["status"]);
         if(basket["status"]!="payed")
-            $("."+parent_id+" ul li:last").append('<button class="abring_buttons_market_pay">pay this basket</button>');
-        $("."+parent_id+" ul li:last *").attr("basket_id",basket["id"]);
+            $("."+parent_id+" table tr:last-child").append('<td><button class="abring_buttons_market_pay">پرداخت این سبد</button></td>');
+        $("."+parent_id+" table tr:last-child * td").attr("basket_id",basket["id"]);
     })
 };
